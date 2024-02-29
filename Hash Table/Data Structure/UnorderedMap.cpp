7c32f182f6b5c9341940009abc31d83d46054645
kalpak92 <kals9seals@gmail.com>
2024-02-29 07:21:51 +0000

    Add implementation of unordered_map with custom hash function

--- Hash Table/Data Structure/UnorderedMap.cpp
+++ Hash Table/Data Structure/UnorderedMap.cpp
@@ -22,5 +22,113 @@
  * However, they may be less efficient for range iteration through a subset of elements.
  * In summary, std::unordered_map provides a powerful and efficient way to manage key-value associations, leveraging the underlying hash table structure. Remember that the keys are unordered, making it suitable for scenarios where sorting is not a requirement.
 */
+
 #include <iostream>
 #include <unordered_map>
+
+/*
+Definition:
+
+template<
+    class Key,
+    class T,
+    class Compare = std::less<Key>,
+    class Allocator = std::allocator<std::pair<const Key, T>>
+> class map;
+*/
+
+struct Edge {
+    unsigned long v1;
+    unsigned long v2;
+
+    Edge() {
+        v1 = -1;
+        v2 = -1;
+    }
+
+    Edge(unsigned long v1, unsigned long v2) : v1(v1), v2(v2) {}
+
+    // Associative containers require a equality operator to check for duplicate keys
+    bool operator==(const Edge& other) const {
+        return v1 == other.v1 && v2 == other.v2;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const Edge& edge) {
+        os << "v1: " << edge.v1 << ", v2: " << edge.v2;
+        return os;
+    }
+};
+
+struct MyHash {
+    std::size_t operator()(const Edge& edge) const noexcept {
+        return edge.v1 + edge.v2;
+    }
+};
+
+int main() {
+    // Key is a string, value is an Edge
+    std::unordered_map<std::string, Edge> mymap;
+
+    // Inserting in the unordered_map
+    Edge e1(1, 1);
+    mymap.insert({"string", e1});
+
+    for (std::unordered_map<std::string, Edge>::iterator it = mymap.begin(); it != mymap.end(); it++) {
+        std::cout << it->first << " => " << it->second << std::endl;
+    }
+
+    // Using range-based for loop
+    for (auto const& pair : mymap) {
+        std::cout << pair.first << " => " << pair.second << std::endl;
+    }
+
+    // Using range-based for loop with structured bindings
+    for (auto const& [key, value] : mymap) {
+        std::cout << key << " => " << value << std::endl;
+    }
+
+    Edge e2(2, 2);
+    Edge e3(3, 3);
+    
+    // Inserting using the subscript operator
+    mymap["edge2"] = e2;
+    mymap["edge3"] = e3;
+
+    for (auto const& [key, value] : mymap) {
+        std::cout << key << " => " << value << std::endl;
+    }
+
+    // Erase some elements
+    mymap.erase("edge2");
+
+    std::unordered_map<std::string, Edge>::iterator it = mymap.begin();
+    mymap.erase(it);
+
+    for (auto const& [key, value] : mymap) {
+        std::cout << key << " => " << value << std::endl;
+    }
+
+    Edge e4(99, 100);
+    mymap["edge3"] = e4;
+    for (auto const& [key, value] : mymap) {
+        std::cout << key << " => " << value << std::endl;
+    }
+
+    if (mymap.contains("edge3")) {
+        std::cout << "Map contains edge3" << std::endl;
+    }
+
+    // Statistics of unordered_map hashtable
+    std::cout << "mymap has " << mymap.bucket_count() << " buckets." << std::endl;
+    std::cout << "mymap has " << mymap.load_factor() << " load factor." << std::endl;
+
+    std::unordered_map<Edge, std::string, MyHash> mymap2;
+    Edge edgeTest(100, 100);
+    mymap2.insert({edgeTest, "Edge 100"});
+
+    for (auto const& [key, value] : mymap2) {
+        std::cout << key << " => " << value << std::endl;
+    }
+
+    return 0;
+}
