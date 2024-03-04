8f8e2f9dee9de51b8f70b4e0d77d95c633e016ea
kalpak92 <kals9seals@gmail.com>
2024-03-04 00:17:15 -0800

    basic linked list

---
+++ All Things C++/Systems Programming/C Fundamentals/Data Structures/manualList.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node_st {
+    int element;
+    struct node_st *next;
+} Node;
+
+int main(int argc, char **argv) {
+    Node n1, n2;
+
+    n1.element = 1;
+    n1.next = &n2;
+    n2.element = 2;
+    n2.next = NULL;
+    
+    return EXIT_SUCCESS;
+}
