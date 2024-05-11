67f38acc03ab5b150061deb972fed8f5539e1414
kalpak92 <kals9seals@gmail.com>
2024-05-11 16:07:05 -0700

    feat: Implement K closest points to origin using nth_element

--- Priority Queue/KClosestPointsToOrigin.cpp
+++ Priority Queue/KClosestPointsToOrigin.cpp
@@ -44,13 +44,35 @@ public:
      * void partial_sort( RandomIt first, RandomIt middle, RandomIt last, Compare comp );
      *      Elements are sorted with respect to comp.
     */
-   
+
     vector<vector<int>> kClosestUsingPartialSort(vector<vector<int>>& points, int k) {
         auto euclidDistanceCompare = [](vector<int>& p, vector<int>& q) {
             return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
         };
         partial_sort(points.begin(), points.begin() + k, points.end(), euclidDistanceCompare);
 
+        return vector<vector<int>>(points.begin(), points.begin() + k);
+    }
+
+    /**
+     * Approach: Using nth_element
+     * nth_element is a partial sorting algorithm that rearranges elements in [first, last) 
+     * such that the element pointed at by nth is changed to whatever element would be in that position 
+     * if [first, last) was sorted.
+     * 
+     * The order of the elements in the two partitions is unspecified.
+     * 
+     * template< class RandomIt, class Compare >
+     * void nth_element( RandomIt first, RandomIt nth, RandomIt last, Compare comp );
+     *      Elements are sorted with respect to comp.
+    */
+    vector<vector<int>> kClosestUsingNthElement(vector<vector<int>>& points, int k) {
+        auto euclidDistanceCompare = [](vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        };
+        nth_element(points.begin(), points.begin() + k - 1, points.end(), euclidDistanceCompare);
+
+        // can return the output in any order
         return vector<vector<int>>(points.begin(), points.begin() + k);
     }
 };
@@ -76,6 +98,14 @@ int main() {
     printVector(points);
     cout << "The k closest points to the origin are: ";
     printVector(s.kClosestUsingPartialSort(points, k));
+
+    points = {{-2, 2}, {1, 3}};
+    k = 1;
+    cout << "Given points: ";
+    printVector(points);
+    cout << "The k closest points to the origin are: ";
+    printVector(s.kClosestUsingNthElement(points, k));
+    
 
     return 0;
 }
